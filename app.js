032157b28f8e3c1709a5874038c1e7a07e22be8d
morettisf <morettisf@gmail.com>
2016-11-13 22:59:51 -0800

    media queries, fixed media query nav resize animation

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function initMap() {
 
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: city,
-		zoom: 14,
+		zoom: 13,
         disableDefaultUI: true,
 	})
 
--- nav.js
+++ nav.js
@@ -1,17 +1,29 @@
 $(document).ready(function() {
+	$('nav, #hamburger').addClass('animateStart')
 	$('nav').toggleClass('nav-open')
 	$('#hamburger').toggleClass('hamburger-open')
+	setTimeout(function() {
+		$('nav, #hamburger').removeClass('animateStart')
+	}, 1500)
 })
 
 $('#hamburger').click(function() {
+	$('nav, #hamburger').addClass('animateNav')
 	$('nav').toggleClass('nav-open')
 	$('#hamburger').toggleClass('hamburger-open')
+	setTimeout(function() {
+		$('nav, #hamburger').removeClass('animateNav')
+	}, 1500)
 })
 
 $('#go').click(function() {
 	if ((policeChecked === true || fireChecked === true || hospitalChecked === true || mentalChecked === true || vetChecked === true) && address.value) {
+		$('nav, #hamburger').addClass('animateNav')
 		$('nav').toggleClass('nav-open')
 		$('#hamburger').toggleClass('hamburger-open')
+		setTimeout(function() {
+			$('nav, #hamburger').removeClass('animateNav')
+		}, 1500)
 		$('header').fadeOut('500')
 	}
 	else {
