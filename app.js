2229ca29b50343fd146707de675631083ba214cd
morettisf <morettisf@gmail.com>
2016-11-15 20:40:01 -0800

    zoom control

--- app.js
+++ app.js
@@ -83,7 +83,8 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: city,
         zoom: 13,
-        disableDefaultUI: true
+        disableDefaultUI: true,
+        zoomControl: true,
     })
 
     // default search options
