2d1983f1e3aceaeeea7d661a32f26bc35ffc9c17
morettisf <morettisf@gmail.com>
2017-02-14 15:48:12 -0800

    zoom2

--- build/help-hub.1.0.0.js
+++ build/help-hub.1.0.0.js
@@ -150,7 +150,7 @@
 	
 	        map = new google.maps.Map(document.getElementById('map'), {
 	            center: city,
-	            zoom: 14,
+	            zoom: 11,
 	            disableDefaultUI: true,
 	            zoomControl: true
 	        });
--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@ module.exports = function(opts) {
 
         map = new google.maps.Map(document.getElementById('map'), {
             center: city,
-            zoom: 14,
+            zoom: 11,
             disableDefaultUI: true,
             zoomControl: true,
         })
