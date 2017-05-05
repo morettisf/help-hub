879049613f1dcf816a19d011362d064c10bb23e6
morettisf <morettisf@gmail.com>
2017-05-04 21:12:11 -0700

    shake adjustment, hover on btns2

--- build/help-hub.1.0.0.js
+++ build/help-hub.1.0.0.js
@@ -305,7 +305,7 @@
 			mentalChecked = $('#mental').prop('checked');
 			vetChecked = $('#veterinary').prop('checked');
 	
-			if (policeChecked === true || fireChecked === true || hospitalChecked === true || mentalChecked === true || vetChecked === true && address.value) {
+			if ((policeChecked === true || fireChecked === true || hospitalChecked === true || mentalChecked === true || vetChecked === true) && address.value) {
 				$('nav, #hamburger').addClass('animateNav');
 				$('nav').toggleClass('nav-open');
 				$('#hamburger').toggleClass('hamburger-open');
